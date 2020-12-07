9a57f40cf0121470e7f9e32252cd051939df4dbe
Saran Sappa <saran.sappa@gmail.com>
2020-12-07 09:11:33 +0530

    minor bug fixes

--- ticket_booking/routes/index.js
+++ ticket_booking/routes/index.js
@@ -29,7 +29,7 @@ router.get('/register',function(req,res){
 
 router.get('/admin_view_ticket_details',function(req,res){
 
-  html = "<!--Ticket Booking--><html><head><script> function check_creds(form) { username = form.username.value; password = form.password.value; if (username == '' && password== '' )alert (\"Please enter credentials\");else if (password == '') alert (\"Please enter Password\");  else if (username == '') alert (\"Please enter Username\"); } </script></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h1>Admin View Ticket Details</h1><form action = \"/check_admin_creds_get_ticket_details\" method = \"POST\" onSubmit=\"return check_creds(this)\">Username: <input type = \"text\" name = \"username\"> <br><br>Password: <input type = \"password\" name = \"password\"> <br><br>Event : <select name=\"event\"><option value=\"default\">Select an event</option>";
+  html = "<!--Ticket Booking--><html><head><title>Admin View Ticket Details</title><script> function check_creds(form) { username = form.username.value; password = form.password.value; if (username == '' && password== '' )alert (\"Please enter credentials\");else if (password == '') alert (\"Please enter Password\");  else if (username == '') alert (\"Please enter Username\"); } </script></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h1>Admin View Ticket Details</h1><form action = \"/check_admin_creds_get_ticket_details\" method = \"POST\" onSubmit=\"return check_creds(this)\">Username: <input type = \"text\" name = \"username\" required> <br><br>Password: <input type = \"password\" name = \"password\" required> <br><br>Event : <select name=\"event\" required><option value=\"default\">Select an event</option>";
   
   var json_file;
 	request.get({
@@ -61,7 +61,7 @@ router.get('/admin_view_ticket_details',function(req,res){
 
 router.get('/admin_remove_event',function(req,res){
 
-  html = "<!--Ticket Booking--><html><head><script> function check_creds(form) { username = form.username.value; password = form.password.value; if (username == '' && password== '' )alert (\"Please enter credentials\");else if (password == '') alert (\"Please enter Password\");  else if (username == '') alert (\"Please enter Username\"); } </script></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h1>Admin Remove Event</h1><form action = \"/check_admin_creds_remove_event\" method = \"POST\" onSubmit=\"return check_creds(this)\">Username: <input type = \"text\" name = \"username\"> <br><br>Password: <input type = \"password\" name = \"password\"> <br><br>Event : <select name=\"event\"><option value=\"default\">Select an event</option>";
+  html = "<!--Ticket Booking--><html><head><title>Admin Remove Event</title><script> function check_creds(form) { username = form.username.value; password = form.password.value; if (username == '' && password== '' )alert (\"Please enter credentials\");else if (password == '') alert (\"Please enter Password\");  else if (username == '') alert (\"Please enter Username\"); } </script></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h1>Admin Remove Event</h1><form action = \"/check_admin_creds_remove_event\" method = \"POST\" onSubmit=\"return check_creds(this)\">Username: <input type = \"text\" name = \"username\" required> <br><br>Password: <input type = \"password\" name = \"password\" required> <br><br>Event : <select name=\"event\" required><option value=\"default\">Select an event</option>";
   
   var json_file;
 	request.get({
@@ -137,7 +137,7 @@ router.post('/register_check',urlencodedParser,function(req,res){
 router.post('/login_check',urlencodedParser,function(req,res){
   console.log(req.body.username);
   console.log(req.body.password);
-  event_html = "<!--Event--><meta charset=\"UTF-8\"><html> <head><title>Event selection</title></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><form action = \"/event_post\" method = \"POST\">Email Id: <input type = \"text\" name = \"email\"> <h5> Note: If you are not able to find an event in the list, it means that the tickets are no longer available.</h5>Event : <select name=\"event\" ><option value=\"default\">Select an event</option>";
+  event_html = "<!--Event--><meta charset=\"UTF-8\"><html> <head><title>Event selection</title></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><form action = \"/event_post\" method = \"POST\">Email Id: <input type = \"text\" name = \"email\" required> <h4 style=\"color:yellow;\"> Note: If you are not able to find an event in the list, it means that the tickets are no longer available.</h4>Event : <select name=\"event\" required><option value=\"default\">Select an event</option>";
 	var json_file;
 	request.get({
 		  headers: {'content-type' : 'application/x-www-form-urlencoded'},
@@ -154,7 +154,7 @@ router.post('/login_check',urlencodedParser,function(req,res){
 			  			event_html+=k;
 		  			}
 		  		}
-		  		event_html+= "</select><input type = \"submit\" value = \"Submit\"></form></center></body></html>"
+		  		event_html+= "</select><br><br><input type = \"submit\" value = \"Submit\"></form></center></body></html>"
 		  		console.log(event_html);
 				var data = "username="+req.body.username + "&password="+req.body.password;
 				  var store = new EventEmitter();
@@ -232,7 +232,7 @@ router.post('/event_post',urlencodedParser, function(req,res){
 							}, function(error_, response_, body_){
 							  	if(!error_){
 							  		console.log(body_);
-									res.send("<meta charset=\"UTF-8\"><html> <head><title>Ticket Details</title></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h4>Ticket Id :"+ body+ "<br><br> <img src=\"https://qrickit.com/api/qr.php?d="+body+"&addtext=SSS+Events&txtcolor=442EFF&fgdcolor=76103C&bgdcolor=C0F912&qrsize=150&t=p&e=m\" alt=\"QR code not displayable please click the link.\"><br><br>Click <a href=https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+body+">here</a> to get the QR code for event entry.<br><br><h4>You will get a mail with these details.</h4><br><br><button><a href=\"/\">Return to main page</a></button></center></body></html>");
+									res.send("<meta charset=\"UTF-8\"><html> <head><title>Ticket Details</title></head><body style=\"background-image: url('https://wallpaperboat.com/wp-content/uploads/2019/10/free-website-background-07.jpg');\"><center><h4>Ticket Id :"+ body+ "<br><br> <img src=\"https://qrickit.com/api/qr.php?d="+body+"&addtext=SSS+Events&txtcolor=442EFF&fgdcolor=76103C&bgdcolor=C0F912&qrsize=150&t=p&e=m\" alt=\"QR code not displayable please click the link.\"><br><br>Click <a href=https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+body+">here</a> to get the QR code for event entry.<br><br><h4 style=\"color:yellow;\">You will get a mail with these details.</h4><br><br><button><a href=\"/\">Return to main page</a></button></center></body></html>");
 							  	}
 							}
 						  );
@@ -260,6 +260,10 @@ router.post('/check_admin_creds_get_ticket_details',urlencodedParser, function(r
 	var password = req.body.password;
 	var event = req.body.event;
 	var data = "event="+event;
+	if(event=="default"){
+		res.send("Please select a valid event.");
+		return;
+	}
 	console.log(data);
 	if(username == "saransappa" && password == "saran532216"){
 		request.post({
